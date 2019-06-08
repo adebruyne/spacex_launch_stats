70de910415d0563a388a614491198bc22ba08547
Aylin DeBruyne <adebruyne88@gmail.com>
2019-06-08 14:53:35 -0400

    adds cors and successfully retrieves data

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, ApolloContext } from 'react-apollo';
+import Launches from './components/Launches'
 import './App.css';
 import logo from './spacex.png';
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
@@ -15,6 +17,7 @@ class App extends Component {
       <ApolloProvider client={client}>
         <div className="container" >
           <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+          <Launches />
         </div>
       </ApolloProvider>
     );
